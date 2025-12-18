bd752504c2210844a83709887ba9294d37e95350
owenlovescoding <iraoyaowen5@gmail.com>
2025-12-18 12:29:10 +0100

    added a simple output

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    // console.log('this is it');
+
+    return 'Welcome to the project';
   }
 }
